1292a3fe6dc652b28ef35519a3147ed099866051
zafar0171 <modzafar28@gmail.com>
2025-12-12 20:19:58 +0530

    VIM files andd SSh files were added

---
+++ vim_pr.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/bash
+
+#Text Editor
+#Based On VI
+#keyboard only
+#Customization through settings and plugins
+
+#copying pastig and deleting texts
+
+
+#MODES
+
+#Normal
+#Insert
+#Visual -certain types of
+#Command line
+
+
+#h -move left
+#j -move down
+#k -move up
+#l -move right
+
+
+y -
+yw -
+yy
+p -
+x - delete a word
+dd -delete a line
